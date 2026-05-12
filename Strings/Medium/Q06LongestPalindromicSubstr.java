bf906aa92e17233e82f178114e7293b32d717a3a
sathya00704 <sathyanarayanansaikrishnan74@gmail.com>
2026-05-12 20:46:18 +0530

    Strings 1Qn

---
+++ Strings/Medium/Q06LongestPalindromicSubstr.java
@@ -0,0 +1,35 @@
+class Q06LongestPalindromicSubstr {
+    public static void main(String[] args) {
+        String s = "babad";
+        if (s == null || s.length() < 2) {
+            System.out.println(s);
+            return;
+        }
+
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandFromCenter(s, i, i);
+            int len2 = expandFromCenter(s, i, i + 1);
+
+            int len = Math.max(len1, len2);
+
+            if (len > end - start + 1) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        System.out.println(s.substring(start, end + 1));
+    }
+
+    private static int expandFromCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() &&
+               s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
