181c6bd21d1b8cbec0677a1c730d653d48c236ad
unknown <sathyanarayanansaikrishnan74@gmail.com>
2026-08-17 23:16:26 +0530

    Rec subseq qn10 added

---
+++ Recursion/Subsequences/Q10Subsets2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Q10Subsets2 {
+    public static List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(nums,0,new ArrayList<>(), result);
+        return result;
+    }
+
+    private static void backtrack(int[] nums, int start, List<Integer> current, List<List<Integer>> result){
+        result.add(new ArrayList<>(current));
+        for(int i=start;i<nums.length;i++){
+            if(i>start && nums[i]==nums[i-1]){
+                continue;
+            }
+            current.add(nums[i]);
+            backtrack(nums,i+1,current,result);
+            current.remove(current.size()-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums={1,2,2};
+        List<List<Integer>> ans = subsetsWithDup(nums);
+        System.out.println(ans);
+    }
+}
