01bd44e2a4e0b4977bd724a14a063a3533bdb835
unknown <sathyanarayanansaikrishnan74@gmail.com>
2026-08-21 19:26:19 +0530

    BM Basics Qn4 added

---
+++ BitManipulation/Basics/Q04CheckPowOf2.java
@@ -0,0 +1,9 @@
+class Q04CheckPowOf2 {
+    public static boolean isPowerOfTwo(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String args[]){
+        System.out.println(isPowerOfTwo(18));
+    }
+}
