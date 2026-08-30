3110c27137ca025f3d9171fb9b3acef843a6eed6
unknown <sathyanarayanansaikrishnan74@gmail.com>
2026-08-30 18:00:39 +0530

    BM IP Completed

---
+++ BitManipulation/InterviewProblems/Q05SingleNumber3.java
@@ -0,0 +1,33 @@
+class Q05SingleNumber3 {    
+    public static int[] singleNumber(int[] nums) {        
+        int xor=0;
+        for(int i: nums){
+            xor=xor^i;
+        }
+        boolean flag = false;
+        int i=0;
+        int diff = xor & -xor;
+        int b=0;
+        int c=0;
+        for(int j: nums){
+            if((j & diff) != 0){
+                b=b^j;
+            }
+            else{
+                c=c^j;
+            }
+        }
+        if(b>c){
+            return new int[]{c,b};
+        }
+        return new int[]{b,c};
+    }
+
+    public static void main(String[] args){
+        int[] nums = new int[]{3,5,1,2,2,1};
+        int[] ans = singleNumber(nums);
+        for(int i: ans){
+            System.out.print(i+" ");
+        }
+    }
+}
